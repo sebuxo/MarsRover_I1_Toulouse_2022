e30c573b54cd6768f8cdb8a2d43f7131a433ce6e
sebuxo <hell.daoum@gmail.com>
2023-02-07 12:00:03 +0100

    Menu

--- MarsRover/MarsMain.cs
+++ MarsRover/MarsMain.cs
@@ -14,33 +14,43 @@ class MarsMain
                .Orienté(new Orientation())
                .Positionné(0, 0)
                .Build();
+        int stop = 0;
+        String commandLine ="";
 
-        String commandLine = Console.ReadLine();
-        char[] commandsToChars = commandLine.ToCharArray();
-
-        for(int i = 0;i< commandsToChars.Length;i++)
+        do
         {
-            if (commandsToChars[i].ToString().ToLower().Equals("a"))
-            {
-                Console.WriteLine("j'avance vers : " + rover.GetOrientation().GetOrientation());
-                rover.Traiter(new AvancerCommande());
-                Console.WriteLine("ma position "+rover.getCoordonnees());
-                continue;
-            }
-            if (commandsToChars[i].ToString().ToLower().Equals("g"))
-            {
-                rover.Traiter(new TourneAGaucheCommande());
-                Console.WriteLine("je tourne vers  : " + rover.GetOrientation().GetOrientation());
-                continue;
-            }
-            if (commandsToChars[i].ToString().ToLower().Equals("d"))
-            {
-                rover.Traiter(new TournerADroiteCommande());
-                Console.WriteLine("je tourne vers  : " + rover.GetOrientation().GetOrientation());
-                continue;
-            }
-        }
+            Console.WriteLine("Veuillez saisir la liste de commandes que vous voulez faire : (A : avancer , D : tourner droite , G : tourner gauche , Q : Quitter");
+                 commandLine = Console.ReadLine();
+            char[] commandsToChars = commandLine.ToCharArray();
 
+            for (int i = 0; i < commandsToChars.Length; i++)
+            {
+                if (commandsToChars[i].ToString().ToLower().Equals("a"))
+                {
+                    Console.WriteLine("j'avance vers : " + rover.GetOrientation().GetOrientation());
+                    rover.Traiter(new AvancerCommande());
+                    Console.WriteLine("ma position " + rover.getCoordonnees());
+                    continue;
+                }
+                if (commandsToChars[i].ToString().ToLower().Equals("g"))
+                {
+                    rover.Traiter(new TourneAGaucheCommande());
+                    Console.WriteLine("je tourne vers  : " + rover.GetOrientation().GetOrientation());
+                    continue;
+                }
+                if (commandsToChars[i].ToString().ToLower().Equals("d"))
+                {
+                    rover.Traiter(new TournerADroiteCommande());
+                    Console.WriteLine("je tourne vers  : " + rover.GetOrientation().GetOrientation());
+                    continue;
+                }
+                if (commandsToChars[i].ToString().ToLower().Equals("q"))
+                {
+                    Console.WriteLine("Le program va s'eteindre");
+                    stop = 1;
+                }
+            }
+        } while (stop != 1);
         
        
 
